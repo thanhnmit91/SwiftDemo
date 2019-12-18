fd30958dc1b12ace975cbb7f3c60db99c866d5a2
Nguyen Minh Thanh <thanh91pro@gmail.com>
2019-12-18 10:58:11 +0700

    Initial commit

---
+++ SwiftDemo/SwiftDemo.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftDemo.swift
+//  SwiftDemo
+//
+//  Created by administrator on 12/18/19.
+//  Copyright © 2019 Vision. All rights reserved.
+//
+
+import Foundation
+
+public class SwiftDemo: NSObject{
+    
+    let abc = "123"
+    
+}
