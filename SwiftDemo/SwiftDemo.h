55ce6765b1bb8b0e24c3d74112f635964894ae5c
Nguyen Minh Thanh <thanh91pro@gmail.com>
2019-12-18 10:44:18 +0700

    Initial Commit

---
+++ SwiftDemo/SwiftDemo.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDemo.h
+//  SwiftDemo
+//
+//  Created by administrator on 12/18/19.
+//  Copyright © 2019 Vision. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftDemo.
+FOUNDATION_EXPORT double SwiftDemoVersionNumber;
+
+//! Project version string for SwiftDemo.
+FOUNDATION_EXPORT const unsigned char SwiftDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDemo/PublicHeader.h>
+
+
